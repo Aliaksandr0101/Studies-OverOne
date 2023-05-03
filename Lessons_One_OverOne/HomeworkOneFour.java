c84a2541bf6d4741d8a3324f271105fef34e41a0
Aliaksandr Komar <Komarthebest1992@gmail.com>
2023-05-04 00:35:06 +0300

    First commit task №1. Part 4

---
+++ Lessons_One_OverOne/HomeworkOneFour.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class HomeworkOneFour {
+    public static void main(String[] args) {
+        /*Условие задачи
+        На вход по даётся расстояние, в километрах и скорость в километрах в час, найти время , затраченное на путь.
+        Пример ввода: 150 50
+        Вывод: 3*/
+        int distance, speed, time;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите расстояние пути в километрах");
+        distance = scanner.nextInt();
+        System.out.println("Введите скорость в километрах");
+        speed = scanner.nextInt();
+        scanner.close();
+        time = distance/speed;
+        System.out.println("Время затраченное на путь " +time+ " ч");
+
+
+    }
+}
