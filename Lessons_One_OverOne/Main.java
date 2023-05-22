16fdd18b5bd7ed3a15ed37862ba3f6f0d81aba45
Aliaksandr Komar <Komarthebest1992@gmail.com>
2023-05-22 17:10:30 +0300

    Homework. Chapter 2 Part 4 new

--- Lessons_One_OverOne/Main.java
+++ Lessons_One_OverOne/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        HomeworkTwoSix homeworkTwoFive = new HomeworkTwoSix();
+        HomeworkTwoFive homeworkTwoFive = new HomeworkTwoFive();
         homeworkTwoFive.check();
     }
 }
