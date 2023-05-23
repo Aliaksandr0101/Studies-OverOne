2122421ef397c0a62eb883d9938ef206db4cc5d5
Aliaksandr Komar <Komarthebest1992@gmail.com>
2023-05-23 21:42:17 +0300

    Homework. Chapter 2 Part 5

--- Lessons_One_OverOne/HoneworkKiril/HomeworkKirillOne.java
+++ Lessons_One_OverOne/HoneworkKiril/HomeworkKirillOne.java
@@ -1,26 +1,28 @@
 package HoneworkKiril;
+
 import java.util.Scanner;
 
-public class numbers {
-    public void check(){
+public class HomeworkKirillOne {
+    public void check() {
         int a, b, c, temp;
         Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter three numbers");
         a = scanner.nextInt();
         b = scanner.nextInt();
         c = scanner.nextInt();
         scanner.close();
 
-        if (b>=c){
+        if (b <= c) {
             temp = b;
             b = c;
             c = temp;
         }
-        if (a>=b) {
-            temp = a;
-            a = b;
-            b = temp;
+        if (a <= b) {
+            temp = b;
+            b = a;
+            a = temp;
         }
-        if (b>=c) {
+        if (b <= c) {
             temp = b;
             b = c;
             c = temp;
@@ -29,6 +31,4 @@ public class numbers {
         System.out.println(b);
         System.out.println(c);
     }
-
-
 }
--- Lessons_One_OverOne/HoneworkKiril/Main.java
+++ Lessons_One_OverOne/HoneworkKiril/Main.java
@@ -2,7 +2,7 @@ package HoneworkKiril;
 
 public class Main {
     public static void main(String[] args) {
-        numbers numbers = new numbers();
-        numbers.check();
+        HomeworkKirillOne homeworkKirillOne = new HomeworkKirillOne();
+        homeworkKirillOne.check();
     }
 }
