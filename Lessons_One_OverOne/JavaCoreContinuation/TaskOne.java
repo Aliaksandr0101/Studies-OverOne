f7eed48fc6d72ab8b8170240ff11703c531f817a
Aliaksandr Komar <Komarthebest1992@gmail.com>
2023-05-28 15:49:57 +0300

    Main.java TaskOne

--- Lessons_One_OverOne/JavaCoreContinuation/TaskOne.java
+++ Lessons_One_OverOne/JavaCoreContinuation/TaskOne.java
@@ -11,11 +11,13 @@ public class TaskOne {
           numberElements = scanner.nextInt();
       } while (numberElements < 0 || numberElements == 0);
       int [] array = new int[numberElements];
+      
       for (int i = 0; i < array.length; i++) {
           System.out.println("enter one array element");
           element = scanner.nextInt();
           array[i] = element;
       }
+      scanner.close();
           for (int j = 1; j < array.length; j++) {
               if (array[j]>array[j-1]){
                   System.out.println("This element is greater than the previous element " + array[j]);
