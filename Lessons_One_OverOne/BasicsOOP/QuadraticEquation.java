79304fb0344e7fd62ade8eb5ff7b51210f203394
Aliaksandr Komar <Komarthebest1992@gmail.com>
2023-06-24 00:19:18 +0300

    Main.java QuadraticEquationTwo

--- Lessons_One_OverOne/BasicsOOP/QuadraticEquation.java
+++ Lessons_One_OverOne/BasicsOOP/QuadraticEquation.java
@@ -2,17 +2,36 @@ package BasicsOOP;
 
 public class QuadraticEquation {
     private int a, b, c, discriminant;
+    private double rootOne, rootTwo;
 
-    public void decide( int a, int b, int c ){
+    public int decide( int a, int b, int c ){
         this.a = a;
         this.b = b;
         this.c = c;
         discriminant = b * b  - 4 * a * c;
-        System.out.printf("%s%d", "discriminant = ", discriminant);
+        return discriminant;
+    }
+
+    public void define( int discriminant ){
+        this.discriminant = discriminant;
+        if ( discriminant > 0 ) {
+           calculate(discriminant);
+        }else {
+            System.out.printf("%s","The equation has no roots because discriminant < 0");
+            System.exit(0);
+        }
+    }
+    public void calculate (int discriminant){
+        rootOne = (-b + Math.sqrt(discriminant)) / 2 * a;
+        rootTwo = (-b - Math.sqrt(discriminant)) / 2 * a;
+        System.out.printf("%s%f%n", "rootOne = ", rootOne);
+        System.out.printf("%s%f", "rootTwo = ", rootTwo);
+
     }
     public static void main(String[] args) {
         QuadraticEquation quadraticEquation = new QuadraticEquation();
-        quadraticEquation.decide(15, -2, -1);
+        quadraticEquation.define(quadraticEquation.decide(9, 7, 4));
+
 
     }
 }
--- Lessons_One_OverOne/Main.java
+++ Lessons_One_OverOne/Main.java
@@ -1,6 +1,4 @@
-import BasicsOOP.QuadraticEquation;
-import BasicsOOP.Student;
-import Phone.Work;
+
 
 public class Main {
 
