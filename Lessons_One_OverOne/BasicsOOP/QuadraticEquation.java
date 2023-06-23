bc5c81d8ddb5142058260f60698ed6e3a6184586
Aliaksandr Komar <Komarthebest1992@gmail.com>
2023-06-23 17:00:37 +0300

    Main.java QuadraticEquation

---
+++ Lessons_One_OverOne/BasicsOOP/QuadraticEquation.java
@@ -0,0 +1,18 @@
+package BasicsOOP;
+
+public class QuadraticEquation {
+    private int a, b, c, discriminant;
+
+    public void decide( int a, int b, int c ){
+        this.a = a;
+        this.b = b;
+        this.c = c;
+        discriminant = b * b  - 4 * a * c;
+        System.out.printf("%s%d", "dicriminant = ", discriminant);
+    }
+    public static void main(String[] args) {
+        QuadraticEquation quadraticEquation = new QuadraticEquation();
+        quadraticEquation.decide(15, -2, -1);
+
+    }
+}
--- Lessons_One_OverOne/Main.java
+++ Lessons_One_OverOne/Main.java
@@ -1,10 +1,8 @@
+import BasicsOOP.QuadraticEquation;
 import BasicsOOP.Student;
 import Phone.Work;
 
 public class Main {
-    public static void main(String[] args) {
 
-
-    }
 }
 
