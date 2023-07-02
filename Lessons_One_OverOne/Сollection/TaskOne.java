c545bf1751563acb7fd87c7d73a8383c72347f5f
Aliaksandr Komar <Komarthebest1992@gmail.com>
2023-07-02 17:24:05 +0300

    Main.java CollectionTaskOne

---
+++ Lessons_One_OverOne/Сollection/TaskOne.java
@@ -0,0 +1,10 @@
+package Сollection;
+
+public class TaskOne {
+    public void check(){
+        String [] anyWord = new String[3];
+        for (int i = 0; i < 5 ; i++) {
+            anyWord[i] = "Java";
+        }
+    }
+}
