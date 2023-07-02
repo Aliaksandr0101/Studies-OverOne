2584b6808c3c78fb42d0b38d68e4211f85f2fe20
Aliaksandr Komar <Komarthebest1992@gmail.com>
2023-07-02 12:26:23 +0300

    Main.java TaskShells

--- Lessons_One_OverOne/Main.java
+++ Lessons_One_OverOne/Main.java
@@ -1,9 +1,8 @@
-import PeculiaritiesOOP.Customer;
 import javafx.scene.shape.Circle;
 
 public class Main {
     public static void main(String[] args) {
-        Circle circle = new Circle(2.5);
+
 
     }
 }
