22a7273a1d70f432f564b135597fc3f218b94189
Aliaksandr Komar <Komarthebest1992@gmail.com>
2023-07-14 23:27:36 +0300

    Main.java CollectionTaskThree

---
+++ Lessons_One_OverOne/Сollection/TaskThree.java
@@ -0,0 +1,34 @@
+package Сollection;
+
+import java.util.Set;
+import java.util.TreeSet;
+
+public class TaskThree {
+    public static void main(String[] args) {
+        Set<String>  anyWords = new TreeSet<>();
+        anyWords.add("earth");
+        anyWords.add("planet");
+        anyWords.add("sea");
+        anyWords.add("oxygen");
+        anyWords.add("bike");
+        anyWords.add("phone");
+        anyWords.add("mouse");
+        anyWords.add("word");
+        anyWords.add("peace");
+        anyWords.add("enough");
+        anyWords.add("car");
+        anyWords.add("fog");
+        anyWords.add("bird");
+        anyWords.add("motivation");
+        anyWords.add("laptop");
+        anyWords.add("window");
+        anyWords.add("pillow");
+        anyWords.add("arm");
+        anyWords.add("hand");
+        anyWords.add("finger");
+        anyWords.add("success");
+        System.out.println(anyWords);
+
+
+    }
+}
