79da12f3985ec1ccb05fffd40119b08d9eebdb08
Aliaksandr Komar <Komarthebest1992@gmail.com>
2023-07-21 23:43:41 +0300

    Main.java CollectionTaskEight

--- Lessons_One_OverOne/Сollection/ExampleSeven.java
+++ Lessons_One_OverOne/Сollection/ExampleSeven.java
@@ -1,30 +1,50 @@
 package Сollection;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Comparator;
 
 public class ExampleSeven {
     public void check(){
+
         ArrayList<Animal> animals = new ArrayList<>();
-        animals.add(new Animal("Lion"));
-        animals.add(new Animal("Dog"));
-        animals.add(new Animal("Elephant"));
-        animals.add(new Animal("Snake"));
-        animals.add(new Animal("Zebra"));
-        animals.add(new Animal("Lion"));
-        Collections.sort(animals);
+        animals.add(new Animal("Lion", "Brown"));
+        animals.add(new Animal("Dog", "Black"));
+        animals.add(new Animal("Dog", "White"));
+        animals.add(new Animal("Elephant", "Grey"));
+        animals.add(new Animal("Snake", "Orange"));
+        animals.add(new Animal("Zebra", "White-black"));
+        animals.add(new Animal("Cat", "Brown"));
+        animals.add(new Animal("Cat", "Orange"));
+        Collections.sort(animals, new ComparatorAnimal());
+
     }
-    public class Animal implements Comparable<Animal>{
+    public class Animal{
         private  String name;
-        public Animal(String name) {
+        private  String color;
+
+        public Animal(String name, String color) {
             this.name = name;
+            this.color = color;
         }
         public String getName() {
             return name;
         }
+        public String getColor() {
+            return color;
+        }
+
+
+    }
+    public class ComparatorAnimal implements Comparator<Animal>{
 
         @Override
-        public int compareTo(Animal animal) {
-            return this.name.compareTo(animal.getName());
+        public int compare(Animal animalOne, Animal animalTwo) {
+           int nameCompare = animalOne.getName().compareTo(animalTwo.getName());
+           if (nameCompare != 0){
+               return nameCompare;
+           } else
+            return animalOne.getColor().compareTo(animalTwo.getColor());
+
         }
     }
 }
